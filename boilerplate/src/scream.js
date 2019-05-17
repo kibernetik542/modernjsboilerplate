9c1b9d8b40ad37ca2142c0d2f75293a6442c88d6
Arzu Suleymanov <arzu.suleamanov@firstlinesoftware.com>
2019-05-17 09:24:28 +0200

    basic ewbpack+babel setup

---
+++ boilerplate/src/scream.js
@@ -0,0 +1,5 @@
+console.log('scream.js')
+
+const scream = (name) => `Hello ${name.toUpperCase()}!`
+
+export { scream as default}
---
+++ boilerplate/src/utilities.js
@@ -0,0 +1,5 @@
+console.log('%c utilities.js', 'background: #222; color: #bada55')
+
+const add = (x, y) => x + y
+const square = x => x * x
+export { add, square as default}
